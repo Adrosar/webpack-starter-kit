eff0007a8aa9bf1f2d464d76039e4f7fdf32fcd8
Adrian Gargula <adriangargula@interia.pl>
2017-12-23 20:23:31 +0100

    Zaktualizowałem wszystkie pakiety.

---
+++ yarn-add.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+yarn add express serve-index && yarn add --dev @types/jquery babel-core babel-loader babel-preset-env babel-preset-es2015 babel-preset-stage-0 base64-image-loader bundle-loader css-loader do-nothing-loader extract-text-webpack-plugin file-loader fs-extra html-minify-loader json-loader less less-loader node-sass null-loader on-build-webpack preprocess-loader raw-loader sass-loader skeleton-loader style-loader ts-loader typescript uglify-loader webpack webpack-bundle-analyzer xml-loader
