8781846ba14ed22982c6770be891bacd49efe7c3
Adrian Gargula <adriangargula@interia.pl>
2017-12-23 19:48:45 +0100

    Zmieniłem trasowanie adresów URL dla testów jednostkowych. Zmieniłem nazwę pliku `/test/index.html` na `/test/runTest.html`. Poprawiłem link do jQuery.

--- tools/server.js
+++ tools/server.js
@@ -32,14 +32,16 @@ const staticOptions = {
 app.use('/', express.static(dir.build, staticOptions));
 app.use('/', express.static(dir.assets, staticOptions));
 app.use('/', express.static(dir.web, staticOptions));
-app.use('/', express.static(dir.test, staticOptions));
+
+app.use('/spec', express.static(path.resolve(dir.test, 'spec'), staticOptions));
+app.use('/', express.static(path.resolve(dir.test, 'assets'), staticOptions));
 
 app.use('/', serveIndex(dir.web, {
     'icons': true
 }));
 
 app.all('/runTest.html', function (req, res, next) {
-    res.sendFile(path.resolve(dir.test, 'index.html'));
+    res.sendFile(path.resolve(dir.test, 'runTest.html'));
 });
 
 app.listen(port, function () {
